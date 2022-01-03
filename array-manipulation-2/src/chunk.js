9648917e28d93f6db657667d422e55744c06b6b3
Simon Kim <mrsimonkim@gmail.com>
2022-01-03 21:40:10 +0000

    JS array practice 2

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,14 +1,11 @@
 /* exported chunk */
 function chunk(array, size) {
-  var newArray = [[subArray] + [finalArray]];
+  var newArray = [[subArray], [finalArray]];
   var subArray = [];
   var finalArray = [];
   if (array.length > size) {
     for (var i = 0; i < size; i++) {
       subArray.push(array[i]);
-    }
-    for (var i = size; i < array.length; i++) {
-      finalArray.push(array[i]);
     }
   } else if (array === 0) {
     newArray.push();
