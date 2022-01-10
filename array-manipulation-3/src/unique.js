edd93e9159fe9765cc80f3c8ce28a01ab0afd30f
Simon Kim <mrsimonkim@gmail.com>
2022-01-10 18:42:17 +0000

    array practice 3.

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,17 @@
 /* exported unique */
+function unique(array) {
+  var uniqueArray = [];
+  var repeated = false;
+  for (var i = 0; i < array.length; i++) {
+    for (var x = 0; x < uniqueArray.length; x++) {
+      if (array[i] === uniqueArray[x]) {
+        repeated = true;
+      }
+    }
+    if (repeated === false) {
+      uniqueArray.push(array[i]);
+    }
+    repeated = false;
+  }
+  return uniqueArray;
+}
