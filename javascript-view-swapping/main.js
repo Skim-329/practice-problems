c03fe3deb1cee94373e176a573b52faa39b0188b
Simon Kim <mrsimonkim@gmail.com>
2022-01-17 16:42:26 +0000

    view swap refactor.

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -1,23 +1,23 @@
-var container = document.querySelector(".tab-container");
-var tabs = document.querySelectorAll(".tab");
-var views = document.querySelectorAll(".view");
+var $container = document.querySelector(".tab-container");
+var $tabs = document.querySelectorAll(".tab");
+var $views = document.querySelectorAll(".view");
 
-container.addEventListener("click", function (event) {
+$container.addEventListener("click", function (event) {
   if (event.target.matches(".tab")) {
-    for (var i = 0; i < tabs.length; i++) {
-      if (tabs[i] === event.target) {
-        tabs[i].className = "tab active";
+    for (var i = 0; i < $tabs.length; i++) {
+      if ($tabs[i] === event.target) {
+        $tabs[i].className = "tab active";
       } else {
-        tabs[i].className = "tab";
+        $tabs[i].className = "tab";
       }
     }
-  }
-  var attribute = event.target.getAttribute("data-view");
-  for (var x = 0; x < views.length; x++) {
-    if (views[x].getAttribute("data-view") === attribute) {
-      views[x].className = "view";
-    } else {
-      views[x].className = "view hidden";
+    var attribute = event.target.getAttribute("data-view");
+    for (var x = 0; x < $views.length; x++) {
+      if ($views[x].getAttribute("data-view") === attribute) {
+        $views[x].className = "view";
+      } else {
+        $views[x].className = "view hidden";
+      }
     }
   }
 });
